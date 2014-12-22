41413bfee3f4bf2729a1fc329ea88c3459d81155
virusdefender <yang.li@zarpe.cn>
2014-12-23 00:33:25 +0800

    add fabfile

--- tools/fabfile.py
+++ tools/fabfile.py
@@ -1,1 +1,17 @@
 # coding=utf-8
+from fabric.api import local, abort, settings, env, cd, run
+from fabric.colors import *
+from fabric.contrib.console import confirm
+
+env.hosts = ["root@tmqdu.com"]
+env.password = "Zarpe2014"
+
+
+def deploy():
+    with cd("/mnt/source/tm"):
+        print green("将在远程仓库下载代码")
+        run("git pull origin master")
+        # del pyc
+        run('find /mnt/source/tm/ -name "*.pyc" | xargs rm -rf')
+
+        print green("代码部署成功！！！")
